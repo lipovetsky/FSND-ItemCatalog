543615852d3e80eff11a7aa4ed4a122d6089bad0
Josh Lipovetsky <lipovetskyj9@gmail.com>
2018-02-13 11:30:53 -0500

    Add imports to main project file, and add main page to templates

--- project.py
+++ project.py
@@ -1,1 +1,22 @@
-#This is where our final project will be written.
+import os
+from flask import Flask, render_template, url_for, redirect, request, jsonify, flash
+from flask_bootstrap import Bootstrap
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from database import Base, Author, Book
+
+app = Flask(__name__)
+Bootstrap(app)
+
+engine = create_engine('sqlite:///greatbooks.db')
+Base = declarative_base()
+Base.metadata.create_all(engine)
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+@app.route('/')
+def showAll():
+    authors = session.query(Author).all()
+    return render_template('index.html', authors = authors)
