8e5602fd340133250caaa7f37ca8ef0aaa41b236
vagrant <vagrant@vagrant.vm>
2018-03-02 17:38:54 +0000

    Pep 8 the Python main project file

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template, url_for, redirect, request, jsonify, flash
+import random
+import string
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flask_bootstrap import Bootstrap
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
@@ -7,12 +9,11 @@ from sqlalchemy import create_engine
 from database import Base, Author, Book, User
 
 from flask import session as login_session
-import random, string
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
 import json
-from flask import make_response
+from flask import make_response, flash
 import requests
 
 CLIENT_ID = json.loads(
@@ -26,8 +27,9 @@ engine = create_engine('sqlite:///greatbookswithusers.db')
 Base = declarative_base()
 Base.metadata.create_all(engine)
 
-DBSession = sessionmaker(bind = engine)
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
 
 @app.route('/login')
 def showLogin():
@@ -35,6 +37,7 @@ def showLogin():
                     for x in xrange(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -55,25 +58,25 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    #Check validity of access token
+    # Check validity of access token
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    #If error, abort
+    # If error, abort
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    #Verify access token used for correct user
+    # Verify access token used for correct user
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(json.dumps("Token's User ID is not the given User ID"), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    #Verify access token is valid for this app
+    # Verify access token is valid for this app
     if result['issued_to'] != CLIENT_ID:
         response = make_response(json.dumps("Token's client ID doesn't match app's"), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -86,15 +89,15 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    #Store access token in session (for later use)
+    # Store access token in session (for later use)
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
     login_session['provider'] = 'google'
 
-    #Get user info
+    # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = {'access_token' : credentials.access_token, 'alt' : 'json'}
-    answer = requests.get(userinfo_url, params = params)
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
 
@@ -112,28 +115,29 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius: '
+    output += '150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("You are now logged in as %s" % login_session['username'])
     return output
 
 
 def createUser(login_session):
-    newUser = User(name = login_session['username'], email=login_session['email'],
-                   picture = login_session['picture'])
+    newUser = User(name=login_session['username'], email=login_session['email'],
+                   picture=login_session['picture'])
     session.add(newUser)
     session.commit()
-    user = session.query(User).filter_by(email = login_session['email']).one()
+    user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
 
 def getUserInfo(user_id):
-    user = session.query(User).filter_by(id = user_id).one()
+    user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
     try:
-        user = session.query(User).filter_by(email = email).one()
+        user = session.query(User).filter_by(email=email).one()
         return user.id
     except:
         return None
@@ -177,8 +181,9 @@ def gdisconnect():
 def showAllAuthors():
     authors = session.query(Author).order_by(Author.last_name).all()
     if 'username' in login_session:
-        return render_template('index.html', authors = authors)
-    return render_template('publicindex.html', authors = authors)
+        return render_template('index.html', authors=authors)
+    return render_template('publicindex.html', authors=authors)
+
 
 @app.route('/json')
 @app.route('/authors/json')
@@ -201,9 +206,9 @@ def showBooksbyAuthorJSON(author):
 
 @app.route('/<author>/<book>/json')
 def showBookJSON(author, book):
-    author = session.query(Author).filter_by(last_name = author.title()).first()
+    author = session.query(Author).filter_by(last_name=author.title()).first()
     if author:
-        book = session.query(Book).filter_by(name = book.title()).first()
+        book = session.query(Book).filter_by(name=book.title()).first()
         if book:
             return jsonify(book.serialize)
     return "Book not found"
@@ -211,13 +216,13 @@ def showBookJSON(author, book):
 
 @app.route('/<author>')
 def showAuthor(author):
-    theAuthor = session.query(Author).filter_by(last_name = author.title()).first()
+    theAuthor = session.query(Author).filter_by(last_name=author.title()).first()
     if theAuthor:
-        books = session.query(Book).filter_by(author_id = theAuthor.id).all()
+        books = session.query(Book).filter_by(author_id=theAuthor.id).all()
         if 'username' in login_session:
             if login_session['user_id'] == theAuthor.user_id:
-                return render_template('authorpage.html', author = theAuthor, books = books)
-        return render_template('publicauthorpage.html', author = theAuthor, books = books)
+                return render_template('authorpage.html', author=theAuthor, books=books)
+        return render_template('publicauthorpage.html', author=theAuthor, books=books)
     else:
         return "Author doesn't exist!"
 
@@ -232,8 +237,8 @@ def newAuthor():
         thePhoto = request.form['picture']
         userID = login_session['user_id']
         if theAuthor and theBio and thePhoto:
-            session.add(Author(last_name = theAuthor, bio = theBio, photo = thePhoto,
-                               user_id = userID))
+            session.add(Author(last_name=theAuthor, bio=theBio, photo=thePhoto,
+                               user_id=userID))
             session.commit()
             flash("Author has been added!")
             return redirect(url_for('showAllAuthors'))
@@ -247,84 +252,84 @@ def newAuthor():
 @app.route('/<author>/edit', methods=['GET', 'POST'])
 # @app.route('/<authorname>/edit')
 def editAuthor(author):
-    author = session.query(Author).filter_by(last_name = author.title()).first()
+    author = session.query(Author).filter_by(last_name=author.title()).first()
     if 'username' not in login_session:
         return redirect('/login')
     elif login_session['user_id'] != author.user_id:
-        return redirect(url_for('showAuthor', author = author.last_name))
+        return redirect(url_for('showAuthor', author=author.last_name))
     if author:
         if request.method == 'POST':
             author.last_name = request.form['authorname'].title()
             author.bio = request.form['bio']
             author.photo = request.form['picture']
             session.commit()
-            return redirect(url_for('showAuthor', author = author.last_name))
+            return redirect(url_for('showAuthor', author=author.last_name))
         else:
-            return render_template('editauthor.html', author = author)
+            return render_template('editauthor.html', author=author)
     else:
         return "Author doesn't exist!"
 
 
 @app.route('/<author>/delete', methods=['GET', 'POST'])
 def deleteAuthor(author):
-    author = session.query(Author).filter_by(last_name = author).first()
+    author = session.query(Author).filter_by(last_name=author).first()
     if 'username' not in login_session:
         return redirect('/login')
     elif login_session['user_id'] != author.user_id:
-        return redirect(url_for('showAuthor', author = author.last_name))
+        return redirect(url_for('showAuthor', author=author.last_name))
     if request.method == 'POST':
         session.delete(author)
         session.commit()
         return redirect(url_for('showAllAuthors'))
-    return render_template('deleteauthor.html', author = author)
+    return render_template('deleteauthor.html', author=author)
 
 
 @app.route('/<author>/<book>')
 def showBook(author, book):
-    author = session.query(Author).filter_by(last_name = author.title()).first()
-    book = session.query(Book).filter_by(name = book.title()).first()
+    author = session.query(Author).filter_by(last_name=author.title()).first()
+    book = session.query(Book).filter_by(name=book.title()).first()
     if author and book and author.id == book.author_id:
         if 'username' in login_session:
             if login_session['user_id'] == book.user_id:
-                return render_template('bookpage.html', author = author, book = book)
-        return render_template('publicbookpage.html', author = author, book = book)
+                return render_template('bookpage.html', author=author, book=book)
+        return render_template('publicbookpage.html', author=author, book=book)
     else:
         return "Book doesn't exist!"
 
 
 @app.route('/<author>/add', methods=['GET', 'POST'])
 def addBook(author):
-    author = session.query(Author).filter_by(last_name = author.title()).first()
+    author = session.query(Author).filter_by(last_name=author.title()).first()
     if 'username' not in login_session:
         return redirect('/login')
     elif login_session['user_id'] != author.user_id:
-        return redirect(url_for('showAuthor', author = author.last_name))
+        return redirect(url_for('showAuthor', author=author.last_name))
     if request.method == 'POST':
         newBook = (request.form['name'].title(), request.form['image'],
-                    request.form['amazon'], request.form['description'])
+                   request.form['amazon'], request.form['description'])
         if newBook:
             userID = login_session['user_id']
-            session.add(Book(name = newBook[0], image = newBook[1],
-            amazon = newBook[2], description = newBook[3], author_id = author.id, user_id = userID))
+            session.add(Book(name=newBook[0], image=newBook[1],
+                             amazon=newBook[2], description=newBook[3], author_id=author.id, user_id=userID))
             session.commit()
             flash("Book has been added!")
-            return redirect(url_for('showBook', author = author.last_name, book = newBook[0]))
+            return redirect(url_for('showBook', author=author.last_name, book=newBook[0]))
         else:
             flash("You need to fill out all the fields!")
-            return render_template('addbook.html', author = author)
+            return render_template('addbook.html', author=author)
 
-    return render_template('addbook.html', author = author)
+    return render_template('addbook.html', author=author)
 
 
 @app.route('/<author>/<book>/edit', methods=['GET', 'POST'])
 def editBook(author, book):
-    author = session.query(Author).filter_by(last_name = author.title()).first()
+    author = session.query(Author).filter_by(last_name=author.title()).first()
     if 'username' not in login_session:
         return redirect('/login')
     elif login_session['user_id'] != author.user_id:
-        return redirect(url_for('showAuthor', author = author.last_name))
+        return redirect(url_for('showAuthor', author=author.last_name))
     allauthors = session.query(Author).all()
-    book = session.query(Book).filter_by(name = book.title()).first()
+    book = session.query(Book).filter_by(name=book.title()).first()
     if author and book:
         if request.method == 'POST':
             book.name = request.form['name'].title()
@@ -332,34 +337,33 @@ def editBook(author, book):
             book.amazon = request.form['amazon']
             book.description = request.form['description']
             thelastname = request.form['author']
-            newauthor = session.query(Author).filter_by(last_name = thelastname).first()
+            newauthor = session.query(Author).filter_by(last_name=thelastname).first()
             book.author_id = newauthor.id
             thelastname = request.form['author']
             session.commit()
-
-            return redirect(url_for('showBook', author = book.author.last_name, book = book.name))
+            return redirect(url_for('showBook', author=book.author.last_name, book=book.name))
         else:
-            return render_template('editbook.html', theauthor = author, author = allauthors, book = book)
+            return render_template('editbook.html', theauthor=author, author=allauthors, book=book)
     else:
         return "Book doesn't exist!"
 
 
 @app.route('/<author>/<book>/delete', methods=['GET', 'POST'])
 def deleteBook(author, book):
-    author = session.query(Author).filter_by(last_name = author.title()).first()
+    author = session.query(Author).filter_by(last_name=author.title()).first()
     if 'username' not in login_session:
         return redirect('/login')
     elif login_session['user_id'] != author.user_id:
-        return redirect(url_for('showAuthor', author = author.last_name))
-    book = session.query(Book).filter_by(name = book).first()
+        return redirect(url_for('showAuthor', author=author.last_name))
+    book = session.query(Book).filter_by(name=book).first()
     if request.method == 'POST':
         session.delete(book)
         session.commit()
-        return redirect(url_for('showAuthor', author = author.last_name))
-    return render_template('deletebook.html', author = author, book = book)
+        return redirect(url_for('showAuthor', author=author.last_name))
+    return render_template('deletebook.html', author=author, book=book)
 
 
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = 'iloveyou'
-    app.run(host='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
